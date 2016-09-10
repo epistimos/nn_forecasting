f6691f8586820efe0a452198304e419e7fc828c1
Jev Kuznetsov <jev.kuznetsov@gmail.com>
2016-09-10 19:05:50 +0200

    improved datasets interface. added factory and list datasets functions

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -3,6 +3,15 @@ from .wrappers import *
 
 datasets = {'AirPassengers': AirPassengers}
 
+
+def list():
+    """ show available datasets """
+    print('Name\t\t\tDescription') 
+    print(70*'-')
+   
+    for k,v in datasets.items():
+        print( "%s\t\t%s" % (k,v.__doc__))
+
 def load(name):
    """ loads a given dataset """
     
